5be598c54476a6c5083fe2e5a2e31a6806bca5d7
nfwyst <nfwyst@gmail.com>
2018-07-25 15:01:11 +0800

    fix generatorRuntime is not define

--- app/main.js
+++ app/main.js
@@ -1,11 +1,29 @@
+import "babel-polyfill";
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import url from 'url';
 import debuggers from 'electron-debug';
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 debuggers();
 
 // main window
 let mainWindow = null;
+let tasks = {
+  data: [],
+  push: function (func) {
+    this.data.push(func);
+    return this;
+  },
+  execu: function() {
+    this.data.forEach(func => {
+      try {
+        func();
+      } catch (e) {
+        throw new Error(`run task error: ${e.message}`);
+      }
+    });
+  }
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -26,7 +44,19 @@ function createWindow() {
   mainWindow.show();
 }
 
-app.on('ready', createWindow);
+async function installExt() {
+  try {
+    const name = await installExtension(REACT_DEVELOPER_TOOLS);
+    console.log(`Added Extension: ${name}`)
+  } catch (e) {
+    console.log(`An error occurred: ${e.message}`);
+  }
+}
+
+// register
+tasks.push(createWindow).push(installExt);
+
+app.on('ready', tasks.execu.bind(tasks));
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit(); 
