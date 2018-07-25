ef677b9f7315950ab77beed409cef53080091500
nfwyst <nfwyst@gmail.com>
2018-07-25 13:53:51 +0800

    sup jsx

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -6,10 +6,23 @@ const base = require("./webpack.base.config");
 const buildPath = path.resolve(__dirname, "./dist");
 
 const renderer = merge(base, {
-  entry: "./app/renderer.js",
+  entry: "./app/renderer.jsx",
   output: {
     filename: "renderer.js",
     path: buildPath
+  },
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: "babel-loader",
+        options: {
+          presets: ["es2017", "react"],
+          plugins: ["transform-class-properties"]
+        }
+      }]
+    }]
   },
   plugins: [
     new HtmlWebpackPlugin({
